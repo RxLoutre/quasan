b1ed750c24ec34995da2dfb5eef67503855dd175
Mw. R.E. Boyer <r.e.boyer@biology.leidenuniv.nl>
2021-05-12 09:03:23 +0000

    Add new file

---
+++ config.sh
@@ -0,0 +1,4 @@
+#********************PARAMETERS**********************
+WORK_DIR="/Users/roxaneboyer/Bioinformatic/data/mbt_genomes/mbt-fake-collection"
+SCRIPTS_DIR="/Users/roxaneboyer/Bioinformatic/scripts/"
+MAIL="r.e.boyer@biologu.leiden-univ.nl"
