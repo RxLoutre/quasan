29bfbccb95ef7ecdade611247caaa7c0a477d8f0
boyerr <r.e.boyer@biology.leidenuniv.nl>
2021-12-14 13:51:33 +0100

    Still testing automatisation of rclone

--- config-local.sh
+++ config-local.sh
@@ -1,7 +1,9 @@
 #********************PARAMETERS**********************
-COLLECTION_DIR="/Users/roxaneboyer/Bioinformatic/data/mbt_genomes/mbt-training-collection"
+COLLECTION_DIR_LOCAL="/Users/roxaneboyer/Bioinformatic/data/mbt_genomes/mbt-training-collection"
+COLLECTION_DIR_REMOTE="RD:IBL_Bioinformatic/Roxane/mbt-genomes/"
 SCRIPTS_DIR="/Users/roxaneboyer/Bioinformatic/scripts/streptidy"
 GENOME_SIZE="7.5m"
+LOG_DIR="/Users/roxaneboyer/Bioinformatic/logs"
 GRAM="pos"
 RESSOURCES="/Users/roxaneboyer/Bioinformatic/ressources"
 GENUS="Streptomyces"
@@ -12,5 +14,5 @@ LOG=$SCRIPTS_DIR"/Streptidy.log"
 #********************FUNCTIONS**********************
 echolog (){
     DATE_LOG=`date +"%Y %m %d %H:%M:%S"`
-    echo -e "$DATE_LOG\t$1" >> $LOG
+    echo -e "[$1] $DATE_LOG\t$2" >> $LOG
 }
